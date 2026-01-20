65fd764cec955c5578c332495dac13de157886c9
HSn <hsn@linux.do>
2026-01-20 14:48:26 +0800

    Remove postbuild and postexport scripts for HTML alias generation

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   output: 'export',
-  trailingSlash: false,
+  trailingSlash: true,
   basePath,
   assetPrefix: basePath ? `${basePath}/` : undefined,
   images: {
